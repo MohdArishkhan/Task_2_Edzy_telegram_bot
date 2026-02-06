1abc7d18d09556147d6907ad5c6e7fcd9a489d62
Mohd Arish Khan <writetoarish7@gmail.com>
2026-02-06 15:33:51 +0530

    server port changed

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import RateLimitConfig from './config/rateLimitConfig';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
 if (!TELEGRAM_BOT_TOKEN) {
